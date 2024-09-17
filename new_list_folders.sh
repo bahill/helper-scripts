03053b1925ff1c03cd667619f952d7e7ff50e326
bahill <bhill@broadinstitute.org>
2024-09-17 16:09:31 -0400

    Update to list the folder size of each subfolder

--- new_list_folders.sh
+++ new_list_folders.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-bucket=broad-dsp-monster-hca-prod-ucsc-storage
-current_date=$(date +%Y%m%d) # get current date in YYYYMMDD format
+bucket=broad-dsp-monster-hca-prod-ebi-storage
+current_date=$(date +%Y%m%d)
 
-for folder in $(gcloud storage ls gs://${bucket})
- do subfolders=$(gcloud storage ls "$folder")
- echo "$subfolders"
+for folder in $(gcloud storage ls gs://${bucket}); do
+ for subfolder in $(gcloud storage ls "$folder"); do
+   size=$(gcloud storage du "$subfolder" --summarize 2>&1 | awk '{print $1/1073741824}') # convert bytes to GB
+   echo "$subfolder,$size"
+ done
 done >> "${bucket}_list_${current_date}.csv"
 
-#TODO add size to top bucket & ability to have input be a list of buckets
+#TODO input a list of buckets
